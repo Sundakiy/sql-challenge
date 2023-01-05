8e1fe1481fd137ff4c24788aa58a813d1fd6b03a
Sunday Akiyesi <akiyesis@gmail.com>
2023-01-05 01:03:37 -0500

    Update repo

---
+++ EmployeeSQL/table_schema.sql
@@ -0,0 +1,106 @@
+-- Drop departments table if exists
+DROP TABLE IF EXISTS departments;
+
+-- Create new departments table
+CREATE TABLE departments (
+    dept_no VARCHAR(100)   NOT NULL,
+    dept_name VARCHAR(100)   NOT NULL,
+    PRIMARY KEY (dept_no)
+);
+-- View the departments table structure without the data
+SELECT *
+FROM departments;
+
+-- Insert data into the departments table 
+
+-- View the departments table
+SELECT *
+FROM departments;
+
+-- Drop employees table if exists
+DROP TABLE IF EXISTS employees;
+
+-- Create new employees table
+
+CREATE TABLE employees(
+    emp_no INT,
+    emp_title_id VARCHAR(10),
+    birth_date DATE,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    sex VARCHAR(1),
+    hire_date DATE,
+    PRIMARY KEY (emp_no),
+    FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+);
+
+-- View the employees table structure without the data
+SELECT *
+FROM employees;
+
+-- Insert data into the employees table 
+
+-- View the employees table
+SELECT *
+FROM employees;
+
+-- Drop titles table if exists
+DROP TABLE IF EXISTS titles;
+
+-- Create new titles table
+create table titles(
+    title_id VARCHAR,
+    title VARCHAR(25),
+    PRIMARY KEY (title_id));
+
+-- View the titles table structure without the data
+SELECT *
+FROM titles;
+
+-- Insert data into the titles table 
+
+-- View the titles table
+SELECT *
+FROM titles;
+
+-- Drop dept_emp table if exists
+DROP TABLE IF EXISTS dept_emp;
+
+create table dept_emp(
+    emp_no INT,
+    dept_no VARCHAR(10),
+	primary key (dept_no,emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+    FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+-- View the dept_emp table structure without the data
+SELECT *
+FROM dept_emp;
+
+-- Insert data into the dept_emp table 
+
+-- View the dept_emp table
+SELECT *
+FROM dept_emp;
+
+-- Drop dept_managers table if exists
+DROP TABLE IF EXISTS dept_managers;
+
+create table dept_manager(
+    dept_no VARCHAR(10),
+    emp_no INT,
+	primary key (dept_no,emp_no),
+     FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	 FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+-- View the dept_managers table structure without the data
+SELECT *
+FROM dept_managers;
+
+-- Insert data into the dept_managers table 
+
+-- View the dept_managers table
+SELECT *
+FROM dept_managers;
